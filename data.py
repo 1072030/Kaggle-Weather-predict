87d3e3d7cd6b94cf9a72b06776eebe5b5eff8d83
1072030 <229jdu@gmail.com>
2022-11-12 18:40:50 +0800

    update

---
+++ data.py
@@ -0,0 +1,5 @@
+import numpy as np
+from pandas import read_csv
+# temp=np.recfromcsv("train.csv")
+# temp2 = read_csv('train.csv')
+# print(temp2)
---
+++ model.py
@@ -0,0 +1,4 @@
+from sklearn.ensemble import RandomForestRegressor # 随机树森林模型
+import joblib # 保存模型为pkl
+from sklearn.metrics import mean_absolute_error # MAE评估方法
+# from ProcessData import ProcessData # 取数据
